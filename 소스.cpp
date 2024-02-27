57b4a30ba3d8761e88b94d03abc43108b99b3f17
Hahaverse <161506051+Hahaverse@users.noreply.github.com>
2024-02-28 03:16:36 +0900

    first_commit
    10개의 임의의 정수(0~9)를 발생 시키고, 숫자의 출현 빈도수를 세는 코드입니다.

---
+++ 소스.cpp
@@ -0,0 +1,56 @@
+//2023111359 최가윤
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void randomNumbers(int inumbers[], int size);
+void randomCounts(int inumbers[], int size, int counts[]);
+
+int main()
+{
+    int i, size = 10;
+    int counts[10] = { 0 };    // 숫자(0~9) 별 출현 빈도 
+    int inumbers[10];
+    srand((unsigned int)time(0));
+
+    randomNumbers(inumbers, size);		// 난수로 배열 초기화
+    randomCounts(inumbers, size, counts);	// 숫자 별 개수 계산
+
+    // 숫자 별 개수 출력
+    cout << "난수: "<<endl;
+    for (i = 0; i < size; i++)
+        cout << setw(5) << inumbers[i];
+    cout << endl << endl;
+
+
+    // 배열 출력
+    cout << "개수: "<<endl;
+    for (i = 0; i < size; i++)
+        cout << setw(5) << i;
+    cout << endl;
+
+    cout <<"    -----------------------------------------------" << endl;
+
+    cout << endl;
+    for (i = 0; i < size; i++)
+        cout << setw(5) << counts[i];
+    cout << endl << endl;
+
+    return 0;
+}
+
+// 난수로 배열 초기화 
+void randomNumbers(int* pnumbers, int size)
+{
+    for (int i = 0; i < size; i++)
+        *(pnumbers + i) = rand() % 10;
+}
+
+// 숫자 별 개수 계산
+void randomCounts(int* pnumbers, int size, int* counts)
+{
+    for (int i = 0; i < size; i++)
+        (*(counts + *(pnumbers + i)))++;
+}
